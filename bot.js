d491d00bf6136a996e512e762c18baa883ea435f
begado <44729229+begado@users.noreply.github.com>
2018-12-27 14:49:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "c";
 var adminprefix = 'c'
 
-const developers = ["444972118174859264","id"]
+const developers = ["379212980946075648","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
